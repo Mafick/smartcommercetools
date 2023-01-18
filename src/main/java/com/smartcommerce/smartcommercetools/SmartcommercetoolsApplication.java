02f4aac64804a50da93d10a9749d976074343c4d
Niels Horn <niels.horn@smartcommerce.de>
2023-01-18 12:22:11 +0100

    add project/ mvn/ spring initial stuff

---
+++ src/main/java/com/smartcommerce/smartcommercetools/SmartcommercetoolsApplication.java
@@ -0,0 +1,13 @@
+package com.smartcommerce.smartcommercetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcommercetoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcommercetoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcommerce/smartcommercetools/SmartcommercetoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcommerce.smartcommercetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcommercetoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
